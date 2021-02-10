858578e5a75f1cd52592d10da170920bb87dbed6
Jonks <anastasiablackhold@gmail.com>
2021-02-10 21:12:42 +0000

    testing features, some refactoring

--- client.js
+++ client.js
@@ -12,10 +12,15 @@ const connect = function() {
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
-  
+
+  conn.on('connect', () => {
+    console.log(`Success! You're connected to the game server.`);
+    conn.write('Name: AIB');
+
+
   return conn;
 }
 
 module.exports = {
-  connect,
+  connect
 };
